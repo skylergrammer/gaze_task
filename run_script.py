dbe476ad760bd321de22a019b16623435e3885a2
Skyler Grammer <skylergrammer@gmail.com>
2015-11-11 17:01:25 -0600

    args are borken

--- run_script.py
+++ run_script.py
@@ -1,9 +1,23 @@
 from gaze_task import Task
 import tkinter as tk
-from datetime import datetime
+import argparse
 
 
-root = tk.Tk()
-task = Task(root, ['red.png', 'blue.jpg'], n_iters=100, height=400, width=400)
-task.start()
-root.mainloop()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n_iters', default=100)
+    parser.add_argument('--height', default=500)
+    parser.add_argument('--width', default=500)
+    args = parser.parse_args()
+
+    print(args)
+    root = tk.Tk()
+    task = Task(root, ['red.png', 'blue.jpg'],
+                'n_iters'=args.n_iters,
+                'height'=args.height,
+                'width'=args.width)
+    task.start()
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
