ff6806de8138dd1044aee77d10ae4a9cf42acc78
Skyler Grammer <skylergrammer@gmail.com>
2015-11-12 16:22:34 -0600

    gui support

--- gaze_task/__init__.py
+++ gaze_task/__init__.py
@@ -25,14 +25,19 @@ class Task(object):
                width of canvas in number of pixels
     '''
     def __init__(self, root, images,
-                 width=500, height=500, n_iters=500, delta_t=100):
+                 width=500, height=500, autosize=False,
+                 n_iters=500, delta_t=100):
+
         self.root = root
-        self.images = [ImageTk.PhotoImage(Image.open(images[0])),
-                       ImageTk.PhotoImage(Image.open(images[1]))]
+        images_open = [Image.open(x) for x in images]
+        self.images = [ImageTk.PhotoImage(x) for x in images_open]
         self.delta_t = delta_t
         self.n_iters = n_iters
 
-        self.n_images = len(self.images)
+        # If autosize, then get the dimensions from the first image
+        if autosize:
+            width, height = images_open[0].size
+
         self.canvas = tk.Canvas(self.root, width=width, height=height)
         self.canvas.pack(expand=tk.YES, fill=tk.BOTH)
 
@@ -40,30 +45,33 @@ class Task(object):
                                             image=self.images[0])
         self.iter_ = 0
 
-    def flash(self):
-        '''Flashes between the images in images list every delta_t milliseconds.
+    def _flash(self):
+
+        '''Hidden method that flashes between the images in images list every
+           delta_t milliseconds.
         '''
-        self.is_focused = True # fake_eye_tracker(self.iter_)
+        # TODO used to store the results of the eye tracking poll
+        self.is_focused = True
 
         if self.iter_ < self.n_iters:
             if self.is_focused:
                 self.iter_ += 1
                 # Get next image to display
-                new_image = self.images[self.iter_ % self.n_images]
+                new_image = self.images[self.iter_ % 2]
                 self.last_image = new_image
-
                 # Switch to next image after delta_t milliseconds
                 self.canvas.itemconfig(self.img, image=new_image)
-                self.canvas.after(self.delta_t, self.flash)
+                self.canvas.after(self.delta_t, self._flash)
             else:
                 self.canvas.itemconfig(self.img, image=self.last_image)
-                self.canvas.after(self.delta_t, self.flash)
-
+                self.canvas.after(self.delta_t, self._flash)
         else:
             sys.exit()
 
     def start(self):
-        self.flash()
+        '''Calls the hidden _flash() method.
+        '''
+        self._flash()
 
 
 def fake_eye_tracker(iter_):
--- run_script.py
+++ run_script.py
@@ -1,21 +1,44 @@
+#!/usr/bin/env python
 from gaze_task import Task
 import tkinter as tk
 import argparse
-
+import sys
+from PyQt4 import QtGui
+from argparseui import ArgparseUi
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--n_iters', default=100)
-    parser.add_argument('--height', default=500)
-    parser.add_argument('--width', default=500)
-    args = parser.parse_args()
+    parser.add_argument('--image1', type=str, required=True,
+                        help='Required: Filename for image 1')
+    parser.add_argument('--image2', type=str, required=True,
+                        help='Required: Filename for image 2')
+    parser.add_argument('--n_iters', type=int, default=100,
+                        help='Number of iterations [default=100]')
+    parser.add_argument('--height', default=500,
+                        help='Height in pixels [default=500]')
+    parser.add_argument('--width', default=500,
+                        help='Width in pixels [default=500]')
+    parser.add_argument('--autosize', action='store_true', default=False,
+                        help='Will automatically find image size.')
+    parser.add_argument('--timing', default=500,
+                        help='Duration displayed for each image in millis.')
+    app = QtGui.QApplication(sys.argv)
+    a = ArgparseUi(parser,
+                   use_save_load_button=True,
+                   window_title='Run Image Flash Task'
+                   )
+    a.show()
+    app.exec_()
+    args = a.parse_args()
 
-    print(args)
+    images = [args.image1, args.image2]
     root = tk.Tk()
-    task = Task(root, ['red.png', 'blue.jpg'],
-                'n_iters'=args.n_iters,
-                'height'=args.height,
-                'width'=args.width)
+    task = Task(root, images,
+                n_iters=args.n_iters,
+                height=args.height,
+                width=args.width,
+                delta_t=args.timing,
+                autosize=args.autosize)
     task.start()
     root.mainloop()
 
