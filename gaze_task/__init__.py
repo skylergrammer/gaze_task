0f414375b74c9966229edb6b7fc29217f6c2be4d
Skyler Grammer <skylergrammer@gmail.com>
2015-11-27 12:37:28 -0600

    updated

--- gaze_task/__init__.py
+++ gaze_task/__init__.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+import Tkinter as tk
 from PIL import ImageTk, Image
 import sys
 
--- run_script.py
+++ run_script.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python
 from gaze_task import Task
-import tkinter as tk
+import Tkinter as tk
 import argparse
 import sys
 from PyQt4 import QtGui
 from argparseui import ArgparseUi
+from pygaze.libtobii import TobiiTracker
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--image1', type=str, required=True,
+    parser.add_argument('image1', type=str,
                         help='Required: Filename for image 1')
-    parser.add_argument('--image2', type=str, required=True,
+    parser.add_argument('image2', type=str,
                         help='Required: Filename for image 2')
     parser.add_argument('--n_iters', type=int, default=100,
                         help='Number of iterations [default=100]')
-    parser.add_argument('--height', default=500,
+    parser.add_argument('--height', default=None,
                         help='Height in pixels [default=500]')
-    parser.add_argument('--width', default=500,
+    parser.add_argument('--width', default=None,
                         help='Width in pixels [default=500]')
-    parser.add_argument('--autosize', action='store_true', default=False,
+    parser.add_argument('--autosize', action='store_true', default=True,
                         help='Will automatically find image size.')
     parser.add_argument('--timing', default=500,
                         help='Duration displayed for each image in millis.')
@@ -30,6 +32,8 @@ def main():
     a.show()
     app.exec_()
     args = a.parse_args()
+    if args.height is not None and args.width is not None:
+        args.autosize = False
 
     images = [args.image1, args.image2]
     root = tk.Tk()
